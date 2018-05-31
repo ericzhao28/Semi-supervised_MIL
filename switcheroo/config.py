00711728ce16bc37f9ef318d54827744efdb2f0d
Eric Zhao <ericlzhao28@gmail.com>
2018-05-31 13:50:42 -0700

    Continuing autoencoder refactor and bug fixes.

---
+++ switcheroo/config.py
@@ -0,0 +1,16 @@
+##############################
+##### Hyperparameters.
+##############################
+
+# TODO: swap to Tensorflow flags
+# to align with rest of MIL lib.
+T_in = 10
+T_pred = 1
+IMG_H = 64
+IMG_W = 64
+IMG_CH = 64
+BATCH = 64
+num_steps = 1000
+N_EPOCH = 10
+std_dev = 0.1
+
--- switcheroo/utils.py
+++ switcheroo/utils.py
@@ -2,14 +2,32 @@ from PIL import Image, ImageSequence
 import numpy as np
 import pickle
 import glob
-from multiprocessing import Pool
+
 
 paths = glob.glob("/home/ubuntu/gym/mil/data/*/*/*.gif")[14800:]
 def gif_to_np(path):
   with open(path[:-4] + ".pkl", "wb") as f:
     im = Image.open(path)
-data = np.array([np.array(frame.copy().convert("RGB").getdata(), dtype=np.uint8).reshape(frame.size[1], frame.size[0], 3) for frame in ImageSequence.Iterator(im)])
+    data = np.array(
+        [np.array(
+            frame.copy().convert("RGB").getdata(), dtype=np.uint8
+         ).reshape(
+            frame.size[1], frame.size[0], 3
+         ) for frame in ImageSequence.Iterator(im)]
+    )
 	  pickle.dump(data,f)
-	 with Pool(processes=3) as pool:
-	 pool.map(gif_to_np, paths, chunksize = 100)
+    with Pool(processes=3) as pool:
+	    pool.map(gif_to_np, paths, chunksize = 100)
+
+
+def load_data():
+  # TODO change input dimensions
+  data = np.reshape(data, [-1, VID_L, IMG_H, IMG_W, IMG_CH])
+  # TODO update output/in to match config flags and our case
+  # Input is the first 10 seconds
+  X = data[:, 0:10]
+  # Output is after 10 seconds.
+  Y = data[:, 10:]
+
+  return X, Y
 
